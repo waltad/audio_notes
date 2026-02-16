83893fe5fcde141cbad898ea19100b69a033dcc2
Tadek <tadek.waluga@gmail.com>
2026-02-16 19:49:36 +0100

    secrets managment

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ from qdrant_client.models import PointStruct, Distance, VectorParams
 env = dotenv_values(".env")
 ## Secrets using Streamlit Cloud Mechanism
 # https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management
-# if 'QDRANT_URL' in st.secrets:
-#     env['QDRANT_URL'] = st.secrets['QDRANT_URL']
-# if 'QDRANT_API_KEY' in st.secrets:
-#     env['QDRANT_API_KEY'] = st.secrets['QDRANT_API_KEY']
+if 'QDRANT_URL' in st.secrets:
+    env['QDRANT_URL'] = st.secrets['QDRANT_URL']
+if 'QDRANT_API_KEY' in st.secrets:
+    env['QDRANT_API_KEY'] = st.secrets['QDRANT_API_KEY']
 ###
 
 EMBEDDING_MODEL = "text-embedding-3-small"
